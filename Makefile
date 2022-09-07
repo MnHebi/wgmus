dcee4c5d42f1b03c250acf9c62fdb8ebd1b3bb21
MnHebi <50542588+MnHebi@users.noreply.github.com>
2022-09-07 13:35:11 +0300

    Add files via upload

--- Makefile
+++ Makefile
@@ -3,8 +3,8 @@ REV=$(shell sh -c 'git rev-parse --short @{0}')
 all: mmusi.dll
 
 
-mmusi.dll: mmusi.c mmusi.def stubs.c
-	mingw32-gcc -std=gnu99 -Wl,--enable-stdcall-fixup -Ilibs/include -O2 -shared -s -o mmusi.dll mmusi.c stubs.c mmusi.def -L. -lvorbisfile-3  -lwinmm -lbass -D_DEBUG -static-libgcc
+mmusi.dll: mmusi.c rwinmm.c mmusi.def
+	mingw32-gcc -std=gnu99 -Wl,--enable-stdcall-fixup -Ilibs/include -O2 -shared -s -o mmusi.dll mmusi.c rwinmm.c mmusi.def -L. -lwinmm -D_DEBUG -static-libgcc
 
 clean:
 	rm -f mmusi.dll
---
+++ rwinmm.c
@@ -0,0 +1,37 @@
+#include <windows.h>
+#include "mmusi.h"
+
+static HINSTANCE winmm = 0;
+
+HINSTANCE getWinmmHandle()
+{
+    return winmm;
+}
+
+/* watches for the app to close, unloads the library when it does */
+/* since FreeLibrary is dangerous in DllMain */
+void ExitMonitor(LPVOID DLLHandle)
+{
+    WaitForSingleObject(DLLHandle, INFINITE);
+    FreeLibrary(getWinmmHandle());
+}
+
+/* if winmm.dll is already loaded, return its handle */
+/* otherwise, load it */
+HINSTANCE loadRealDLL()
+{
+    if (winmm)
+        return winmm;
+
+    char winmm_path[MAX_PATH];
+
+    GetSystemDirectory(winmm_path, MAX_PATH);
+    strncat(winmm_path, "\\winmm.DLL", 11); /* fixed gcc overflow warning */
+
+    winmm = LoadLibrary(winmm_path);
+    
+    /* start watcher thread to close the library */
+    CreateThread(NULL, 500, (LPTHREAD_START_ROUTINE)ExitMonitor, GetCurrentThread(), 0, NULL);
+
+    return winmm;
+}
