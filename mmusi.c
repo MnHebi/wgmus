0e43225c0ff28d26c9c1abecc0c7f2147d04f7a5
MnHebi <50542588+MnHebi@users.noreply.github.com>
2022-09-08 20:04:35 +0300

    working track reading

--- mmusi.c
+++ mmusi.c
@@ -5,6 +5,13 @@
 #include <dirent.h>
 #include <string.h>
 
+#define MAGIC_DEVICEID 0xBEEF
+#define MAX_TRACKS 99
+
+/* PROJECT LIBRARIES START */
+
+/* PROJECT LIBRARIES END */
+
 CRITICAL_SECTION cs;
 
 #define dprintf(...) if (fh) { fprintf(fh, __VA_ARGS__); fflush(NULL); }
@@ -12,12 +19,55 @@ FILE *fh = NULL;
 
 char musdll_path[2048];
 
-/* Config File Defines start */
+/* CONFIG FILE DEFINES START */
+
 int AudioLibrary;
 int FileFormat;
 int PlaybackMode;
 char MusicFolder[255];
-/* Config File Defines end */
+char strFileFormat[5];
+TCHAR MusicFolderFullPath[MAX_PATH];
+char strMusicFile[32];
+TCHAR MusicFileFullPath[MAX_PATH];
+HANDLE findTracks = INVALID_HANDLE_VALUE;
+WIN32_FIND_DATA MusicFiles;
+
+/* defines for storing music data */
+
+struct track_info
+{
+    char path[MAX_PATH];    /* full path to track */
+	int current;
+	int next;
+	int first;
+	int last;	
+};
+
+static struct track_info tracks[MAX_TRACKS];
+
+int firstTrack = -1;
+int lastTrack = 0;
+int numTracks = 1; /* +1 for data track on mixed mode cd's */
+
+/* CONFIG FILE DEFINES END */
+
+/* WAVEOUT DEFINES START */
+
+WAVEFORMATEX waveformat;
+
+/* WAVEOUT DEFINES END */
+
+/* BASS PLAYER DEFINES START 
+HWND win;
+
+HSTREAM *strs;
+int strc;
+HMUSIC *mods;
+int modc;
+HSAMPLE *sams;
+int samc;
+
+ BASS PLAYER DEFINES END */
 
 BOOL FileExists(LPCTSTR szPath)
 {
@@ -51,6 +101,57 @@ void mmusi_config()
 	FileFormat = GetPrivateProfileInt("Settings", "FileFormat", 0, ConfigFileNameFullPath);
 	PlaybackMode = GetPrivateProfileInt("Settings", "PlaybackMode", 0, ConfigFileNameFullPath);
 	GetPrivateProfileString("Settings", "MusicFolder", "tamus", MusicFolder, MAX_PATH, ConfigFileNameFullPath);
+	dprintf("	AudioLibrary = %d\r\n", AudioLibrary);
+	dprintf("	FileFormat = %d\r\n", FileFormat);
+	dprintf("	PlaybackMode = %d\r\n", PlaybackMode);
+	dprintf("	MusicFolder = %s\r\n", MusicFolder);
+	
+	if(FileFormat == 0)
+	{
+		strcpy(strFileFormat, ".wav");
+		dprintf("	File Format is wav\r\n");
+	}
+	else
+	if(FileFormat == 1)
+	{
+		strcpy(strFileFormat, ".mp3");
+		dprintf("	File Format is mp3\r\n");
+	}
+	else
+	if(FileFormat == 2)
+	{
+		strcpy(strFileFormat, ".ogg");
+		dprintf("	File Format is ogg\r\n");
+	}
+	else
+	if(FileFormat == 3)
+	{
+		strcpy(strFileFormat, ".flac");
+		dprintf("	File Format is FLAC\r\n");
+	}
+	else
+	if(FileFormat == 4)
+	{
+		strcpy(strFileFormat, ".aiff");
+		dprintf("	File Format is AIFF\r\n");
+	}
+
+	strcpy(MusicFolderFullPath, musdll_path);
+	*(strrchr(MusicFolderFullPath, '\\')+1)=0;
+	strcat(MusicFolderFullPath, MusicFolder);
+	dprintf("	Reading music files from: %s\r\n", MusicFolderFullPath);
+	strcpy(MusicFileFullPath, MusicFolderFullPath);
+	strcat(MusicFileFullPath, "\\");
+	dprintf("	Music folder is: %s\r\n", MusicFileFullPath);
+	strcpy(strMusicFile, "*");
+	strcat(strMusicFile, strFileFormat);
+	strcat(MusicFileFullPath, strMusicFile);
+	findTracks = FindFirstFileA(MusicFileFullPath, &MusicFiles);
+	do
+	{
+		dprintf("	Music tracks are: %s\r\n", MusicFiles.cFileName);	
+	} while (FindNextFileA(findTracks, &MusicFiles) != 0);
+	FindClose(findTracks);
 
 	return;
 }
@@ -93,18 +194,83 @@ MCIERROR WINAPI mmusi_mciSendCommandA(MCIDEVICEID deviceID, UINT uintMsg, DWORD_
 
 MCIERROR WINAPI mmusi_mciSendStringA(LPCTSTR lpszCmd, LPTSTR lpszRetStr, UINT cchReturn, HANDLE  hwndCallback)
 {
+	MCIERROR err;
+	if(TRUE) 
+	{
+		dprintf("[MCI String = %s]\n", lpszCmd);
+		
+		for (int i = 0; lpszCmd[i]; i++)
+		{
+			tolower(lpszCmd[i]);
+		}
+		
+		if (strstr(lpszCmd, "open cdaudio"))
+		{
+			mmusi_mciSendCommandA(MAGIC_DEVICEID, MCI_OPEN, 0, (DWORD_PTR)NULL);
+			dprintf("	OPEN COMMAND SENT\r\n");
+			return 0;
+		}
+		if (strstr(lpszCmd, "pause cdaudio"))
+		{
+			mmusi_mciSendCommandA(MAGIC_DEVICEID, MCI_PAUSE, 0, (DWORD_PTR)NULL);
+			dprintf("	PAUSE COMMAND SENT\r\n");
+			return 0;
+		}
+		if (strstr(lpszCmd, "stop cdaudio"))
+		{
+			mmusi_mciSendCommandA(MAGIC_DEVICEID, MCI_STOP, 0, (DWORD_PTR)NULL);
+			dprintf("	STOP COMMAND SENT\r\n");
+			return 0;
+		}
+		if (strstr(lpszCmd, "close cdaudio"))
+		{
+			mmusi_mciSendCommandA(MAGIC_DEVICEID, MCI_CLOSE, 0, (DWORD_PTR)NULL);
+			dprintf("	CLOSE COMMAND SENT\r\n");
+			return 0;
+		}
+		if (strstr(lpszCmd, "set cdaudio time format milliseconds"))
+		{
+			static MCI_SET_PARMS parms;
+			parms.dwTimeFormat = MCI_FORMAT_MILLISECONDS;
+			mmusi_mciSendCommandA(MAGIC_DEVICEID, MCI_SET, MCI_SET_TIME_FORMAT, (DWORD_PTR)&parms);
+			dprintf("	TIME FORMAT MILLISECONDS COMMAND SENT\r\n");
+			return 0;
+		}
+		if (strstr(lpszCmd, "set cdaudio time format tmsf"))
+		{
+			static MCI_SET_PARMS parms;
+			parms.dwTimeFormat = MCI_FORMAT_TMSF;
+			mmusi_mciSendCommandA(MAGIC_DEVICEID, MCI_SET, MCI_SET_TIME_FORMAT, (DWORD_PTR)&parms);
+			dprintf("	TIME FORMAT TMSF COMMAND SENT\r\n");
+			return 0;
+		}
+	}
 }
 
 UINT WINAPI mmusi_auxGetNumDevs()
 {
+	return 1;
 }
 
-MMRESULT WINAPI mmusi_auxGetDevCapsA(UINT_PTR uintptrDeviceID, LPAUXCAPSA lpcapsa, UINT cbCaps)
+MMRESULT WINAPI mmusi_auxGetDevCapsA(UINT_PTR uintptrDeviceID, LPAUXCAPSA lpCapsa, UINT cbCaps)
 {
+	dprintf("mmusi_auxGetDevCapsA(uintptrDeviceID=%08X, lpCapsa=%p, cbCaps=%08X\n", uintptrDeviceID, lpCapsa, cbCaps);
+
+	lpCapsa->wMid = 2 /*MM_CREATIVE*/;
+	lpCapsa->wPid = 401 /*MM_CREATIVE_AUX_CD*/;
+	lpCapsa->vDriverVersion = 1;
+	strcpy(lpCapsa->szPname, "mmusi virtual CD");
+	lpCapsa->wTechnology = AUXCAPS_CDAUDIO;
+	lpCapsa->dwSupport = AUXCAPS_VOLUME;
+
+	return MMSYSERR_NOERROR;
 }
 
 MMRESULT WINAPI mmusi_auxGetVolume(UINT uintDeviceID, LPDWORD lpdwVolume)
 {
+	dprintf("mmusi_auxGetVolume(uintDeviceID=%08X, lpdwVolume=%p)\r\n", uintDeviceID, lpdwVolume);
+	*lpdwVolume = 0x00000000;
+	return MMSYSERR_NOERROR;
 }
 
 
@@ -120,6 +286,7 @@ BOOL WINAPI mmusi_PlaySoundA(LPCTSTR lpctstrSound, HMODULE hmod, DWORD dwSound)
 
 UINT WINAPI mmusi_waveOutGetNumDevs()
 {
+	return 1;
 }
 
 MMRESULT WINAPI mmusi_waveOutGetVolume(HWAVEOUT hwo, LPDWORD lpdwVolume)
